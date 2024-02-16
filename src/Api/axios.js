8ac7587f5295efc433666f039de3ee97750da218
yemesrachmeshesha25 <154487046+yemesrachmeshesha25@users.noreply.github.com>
2024-02-16 08:26:50 +0300

    deploy to netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 const axiosInstance = axios.create({
+  //deploy version of firebase function
   // baseURL: "https://api-abyjie4qeq-uc.a.run.app",
-  baseURL: "http://127.0.0.1:5001/clone-18217/us-central1/api",
+
+
+  //local instance of firebase function
+  // baseURL: "http://127.0.0.1:5001/clone-18217/us-central1/api",
+
+
+  //deployed version of amazon server on render.com
+   baseURL: "https://amazon-api-deploy-lxvy.onrender.com/"
+
 });
 export { axiosInstance };
