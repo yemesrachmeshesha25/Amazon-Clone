24d2943b3bb1cf6d39c15f8b808eb89883407252
yemesrachmeshesha25 <154487046+yemesrachmeshesha25@users.noreply.github.com>
2024-02-17 07:12:54 +0300

    bug fixed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -5,11 +5,11 @@ const axiosInstance = axios.create({
 
 
   //local instance of firebase function
-  // baseURL: "http://127.0.0.1:5001/clone-18217/us-central1/api",
+   baseURL: "http://127.0.0.1:5001/clone-18217/us-central1/api",
 
 
   //deployed version of amazon server on render.com
-   baseURL: "https://amazon-api-deploy-lxvy.onrender.com/"
+  //  baseURL: "https://amazon-api-deploy-lxvy.onrender.com/"
 
 });
 export { axiosInstance };
